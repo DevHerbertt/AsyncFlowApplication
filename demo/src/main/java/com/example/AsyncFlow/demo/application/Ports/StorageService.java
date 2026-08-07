23edaf026367826319a20ce05cbce209f44d0eb4
herbert santos <Herbertmosth@gmail.com>
2026-08-07 01:03:42 -0300

    fix: remove pasta demo e limpa arquivos antigos do staging

--- demo/src/main/java/com/example/AsyncFlow/demo/application/Ports/StorageService.java
+++
@@ -1,5 +0,0 @@
-package com.example.AsyncFlow.demo.application.Ports;
-
-public interface StorageService {
-    void storageSave();
-}
--- demo/src/main/java/com/example/AsyncFlow/demo/infrastructure/persistencia/JpaClientRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.AsyncFlow.demo.infrastructure.persistencia;
-
-import com.example.AsyncFlow.demo.Domain.Model.Client;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface JpaClientRepository extends JpaRepository<Client, Long> {
-}
