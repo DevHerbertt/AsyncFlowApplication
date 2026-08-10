511c9f4b9f377ccaba18a30372d6e0d9efb45a17
herbert santos <Herbertmosth@gmail.com>
2026-08-10 15:39:42 -0300

    feat: add validation for items, clean architecture ports and PostgreSQL database setup

---
+++ services/Validation-Worker/src/main/java/com/example/AsyncFlow/demo/infrastructure/persistencia/NotaFiscalStatusRespositoryImpl.java
@@ -0,0 +1,18 @@
+package com.example.AsyncFlow.demo.infrastructure.persistencia;
+
+import com.example.AsyncFlow.contracts.NotaFiscalStatus;
+import com.example.AsyncFlow.demo.application.Ports.out.NotaFiscalStatusRepository;
+
+public class NotaFiscalStatusRespositoryImpl implements NotaFiscalStatusRepository {
+
+
+    @Override
+    public NotaFiscalStatus saveStatus(String numeroNota, NotaFiscalStatus status) {
+        try {
+
+        }catch (Exception e){
+
+        }
+        return null;
+    }
+}
